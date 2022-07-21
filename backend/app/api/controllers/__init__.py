049802504fd028920c0f4cead8f8a90e9e5624f6
quebim <kevinledesmam95@gmail.com>
2022-07-20 22:07:24 -0300

    Added auth controller

--- backend/app/api/controllers/__init__.py
+++ backend/app/api/controllers/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
  
-from . import coffee_brew, coffee_maker
+from . import auth, coffee_brew, coffee_maker
 
 
 router = APIRouter(prefix='/api')
 
+router.include_router(auth.router, tags=['auth'])
 router.include_router(coffee_maker.router, tags=['coffee_maker'])
 router.include_router(coffee_brew.router, tags=['coffee_brew'])
